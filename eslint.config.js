5d795564bf62ee3df752bcd0b4b405a75f58389d
Nayan Patel <nayank@mobifyi.com>
2025-05-31 11:28:43 +0530

    Fix: Remove unwanted packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-const eslint = require('@eslint/js')
+// const eslint = require('@eslint/js')
 const tsPlugin = require('@typescript-eslint/eslint-plugin')
 const tsParser = require('@typescript-eslint/parser')
 
@@ -10,24 +10,24 @@ module.exports = [
       parserOptions: {
         project: './tsconfig.json',
         ecmaVersion: 2021,
-        sourceType: 'module',
-      },
+        sourceType: 'module'
+      }
     },
     plugins: {
-      '@typescript-eslint': tsPlugin,
+      '@typescript-eslint': tsPlugin
     },
     rules: {
       ...tsPlugin.configs.recommended.rules,
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      'semi': ['error', 'never'],
-    //   'no-console': 'warn',
+      semi: ['error', 'never']
+      // 'no-console': 'warn',
     },
-    ignores: ['dist/**', 'node_modules/**'],
-  },
-//   {
-//     files: ['**/*.js'],
-//     ...eslint.configs.recommended,
-//   },
-] 
+    ignores: ['dist/**', 'node_modules/**']
+  }
+  //   {
+  //     files: ['**/*.js'],
+  //     ...eslint.configs.recommended,
+  //   },
+]
